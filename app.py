9786e9ac0abb6213178b41f563d6e46a9746972a
Corey Gallagher <gallacor@oregonstate.edu>
2021-10-27 18:16:51 -0400

    Connect Stylesheet to all pages and refactor state and country requests into two separate forms

--- app.py
+++ app.py
@@ -9,13 +9,9 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/state_or_country", methods=['POST'])
-def state_or_country():
-    try:
-        if request.form["country"] == "True":
-            return country_page_unsorted()
-    except KeyError:
-        return state_page()
+@app.route("/state_data", methods=['POST'])
+def state_data():
+    return state_page()
 
 def state_page():
     state = request.form["state"]
@@ -26,6 +22,9 @@ def state_page():
     forest_density = str(response_forest_data())
     return render_template('state_page.html', poverty_rate=poverty_rate, gini_index=gini_index, state=state, forest_density=forest_density)
 
+@app.route("/country_view", methods=['POST'])
+def country_view():
+        return country_page_unsorted()
 
 def country_page_unsorted():
     # create matrix for 50 states data and generate table
